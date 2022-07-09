05f04857f4f860cfd45c08e03b2f7bd60e144ced
Raferna <raferna.dg@gmail.com>
2022-07-09 12:24:29 -0300

    V6 Fetch

--- Desafíos/js/simul.js
+++ Desafíos/js/simul.js
@@ -1,7 +1,7 @@
-let user;
-let userLS = JSON.stringify(localStorage.getItem("user"))
+// let user;
+// let userLS = JSON.stringify(localStorage.getItem("user"))
 
-user = userLS ? alert("Bienvenido") : prompt("Ingresa tu nombre de usuario")
+// user = userLS ? alert("Bienvenido") : prompt("Ingresa tu nombre de usuario")
 
 //Distribución de especialistas en consultorio médico
 const especialidades = [
@@ -32,72 +32,86 @@ let imputTarde = document.getElementById("flexradioTarde");
 
 // Info sobre especialidad seleccionada
 let result = document.getElementById("resultadoCons");
-{/* Turnos disponibles para especialidad seleccionada */}
+/* Turnos disponibles para especialidad seleccionada */
 let planilla = document.getElementById("planillaTurnos");
+// Loader
+let animLoader = document.getElementById("animLoader");
 
 imputCardio.addEventListener(`change`,() => {
-    result.innerHTML = `<h4>Especialidad: ${especialidades[0].Nombre}</h4>
-    <p>Consultorio: ${especialidades[0].Consultorio}
-    <p>Total de horas diarias: ${(especialidades[0].Horas_dia) + (especialidades[0].Horas_tarde)}
-    <p>Paga diaria: $ ${150 * ((especialidades[0].Horas_dia) + (especialidades[0].Horas_tarde))}`
-    planilla.innerHTML = `<table class="table table-sm table-success table-bordered caption-top">
-    <caption>Turnos disponibles</caption>
-    <thead>
-      <tr>
-        <th scope="col"></th>
-        <th scope="col">08</th>
-        <th scope="col">09</th>
-        <th scope="col">10</th>
-        <th scope="col">11</th>
-        <th scope="col">12</th>
-        <th scope="col">13</th>
-        <th scope="col">14</th>
-        <th scope="col">15</th>
-        <th scope="col">16</th>
-        <th scope="col">17</th>
-        <th scope="col">18</th>
-        <th scope="col">19</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr>
-        <th scope="row">:00</th>
-        <td id="08:00"></td>
-        <td id="09:00"></td>
-        <td id="10:00"></td>
-        <td id="11:00"></td>
-        <td id="12:00"></td>
-        <td id="13:00"></td>
-        <td id="14:00"></td>
-        <td id="15:00">X</td>
-        <td id="16:00"></td>
-        <td id="17:00">X</td>
-        <td id="18:00">X</td>
-        <td id="19:00">X</td>
-      </tr>
-
-      <tr>
-        <th scope="row">:30</th>
-        <td id="08:30"></td>
-        <td id="09:30"></td>
-        <td id="10:30"></td>
-        <td id="11:30"></td>
-        <td id="12:30"></td>
-        <td id="13:30"></td>
-        <td id="14:30"></td>
-        <td id="15:30"></td>
-        <td id="16:30">X</td>
-        <td id="17:30">X</td>
-        <td id="18:30">X</td>
-        <td id="19:30">X</td>
-      </tr>
-    </tbody>
-  </table>
-`
+    animLoader.innerHTML = `<div class="d-flex align-items-center">
+      <strong>Cargando...</strong>
+      <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+      </div>`
+    setTimeout(() => {
+      result.innerHTML = `<h4>Especialidad: ${especialidades[0].Nombre}</h4>
+      <p>Consultorio: ${especialidades[0].Consultorio}
+      <p>Total de horas diarias: ${(especialidades[0].Horas_dia) + (especialidades[0].Horas_tarde)}
+      <p>Paga diaria: $ ${150 * ((especialidades[0].Horas_dia) + (especialidades[0].Horas_tarde))}`
+      planilla.innerHTML = `<table class="table table-sm table-success table-bordered caption-top">
+      <caption>Turnos disponibles</caption>
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">08</th>
+          <th scope="col">09</th>
+          <th scope="col">10</th>
+          <th scope="col">11</th>
+          <th scope="col">12</th>
+          <th scope="col">13</th>
+          <th scope="col">14</th>
+          <th scope="col">15</th>
+          <th scope="col">16</th>
+          <th scope="col">17</th>
+          <th scope="col">18</th>
+          <th scope="col">19</th>
+        </tr>
+      </thead>
+  
+      <tbody>
+        <tr>
+          <th scope="row">:00</th>
+          <td id="08:00"></td>
+          <td id="09:00"></td>
+          <td id="10:00"></td>
+          <td id="11:00"></td>
+          <td id="12:00"></td>
+          <td id="13:00"></td>
+          <td id="14:00"></td>
+          <td id="15:00">X</td>
+          <td id="16:00"></td>
+          <td id="17:00">X</td>
+          <td id="18:00">X</td>
+          <td id="19:00">X</td>
+        </tr>
+  
+        <tr>
+          <th scope="row">:30</th>
+          <td id="08:30"></td>
+          <td id="09:30"></td>
+          <td id="10:30"></td>
+          <td id="11:30"></td>
+          <td id="12:30"></td>
+          <td id="13:30"></td>
+          <td id="14:30"></td>
+          <td id="15:30"></td>
+          <td id="16:30">X</td>
+          <td id="17:30">X</td>
+          <td id="18:30">X</td>
+          <td id="19:30">X</td>
+        </tr>
+      </tbody>
+    </table>
+  `
+      animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputGineco.addEventListener(`change`,() => {
+    animLoader.innerHTML = `<div class="d-flex align-items-center">
+    <strong>Cargando...</strong>
+    <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+    </div>`
+  setTimeout(() => {
     result.innerHTML = `<h4>Especialidad: ${especialidades[1].Nombre}</h4>
     <p>Consultorio: ${especialidades[1].Consultorio}</p>
     <p>Total de horas diarias: ${(especialidades[1].Horas_dia) + (especialidades[1].Horas_tarde)}
@@ -157,9 +171,16 @@ imputGineco.addEventListener(`change`,() => {
     </tbody>
   </table>
 `
+    animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputTraumato.addEventListener(`change`,() => {
+     animLoader.innerHTML = `<div class="d-flex align-items-center">
+      <strong>Cargando...</strong>
+      <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+      </div>`
+    setTimeout(() => {
     result.innerHTML = `<h4>Especialidad: ${especialidades[2].Nombre}</h4>
     <p>Consultorio: ${especialidades[2].Consultorio}</p>
     <p>Total de horas diarias: ${(especialidades[2].Horas_dia) + (especialidades[2].Horas_tarde)}
@@ -219,9 +240,17 @@ imputTraumato.addEventListener(`change`,() => {
     </tbody>
   </table>
 `
+        animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputMedGral.addEventListener(`change`,() => {
+    animLoader.innerHTML = `<div class="d-flex align-items-center">
+      <strong>Cargando...</strong>
+      <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+      </div>`
+
+    setTimeout(() => {
     result.innerHTML = `<h4>Especialidad: ${especialidades[3].Nombre}</h4>
     <p>Consultorio: ${especialidades[3].Consultorio}</p>
     <p>Total de horas diarias: ${(especialidades[3].Horas_dia) + (especialidades[3].Horas_tarde)}
@@ -281,9 +310,17 @@ imputMedGral.addEventListener(`change`,() => {
     </tbody>
   </table>
 `
+    animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputEco.addEventListener(`change`,() => {
+    animLoader.innerHTML = `<div class="d-flex align-items-center">
+      <strong>Cargando...</strong>
+      <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+      </div>`
+
+    setTimeout(() => {
     result.innerHTML = `<h4>Especialidad: ${especialidades[4].Nombre}</h4>
     <p>Consultorio: ${especialidades[4].Consultorio}</p>
     <p>Total de horas diarias: ${(especialidades[4].Horas_dia) + (especialidades[4].Horas_tarde)}
@@ -343,9 +380,17 @@ imputEco.addEventListener(`change`,() => {
     </tbody>
   </table>
 `
+    animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputOft.addEventListener(`change`,() => {
+    animLoader.innerHTML = `<div class="d-flex align-items-center">
+      <strong>Cargando...</strong>
+      <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+      </div>`
+
+    setTimeout(() => {
     result.innerHTML = `<h4>Especialidad: ${especialidades[5].Nombre}</h4>
     <p>Consultorio: ${especialidades[5].Consultorio}</p>
     <p>Total de horas diarias: ${(especialidades[5].Horas_dia) + (especialidades[5].Horas_tarde)}
@@ -405,18 +450,38 @@ imputOft.addEventListener(`change`,() => {
     </tbody>
   </table>
 `
+    animLoader.innerHTML = ""
+    }, 3000);
 })
 
 imputManiana.addEventListener(`change`,() => {
+  animLoader.innerHTML = `<div class="d-flex align-items-center">
+  <strong>Cargando...</strong>
+  <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+  </div>`
+
+setTimeout(() => {
     result.innerHTML = `<h4>Consultorios libres por la mañana</h4>
     <p>${consLibresMañ}</p>`
     planilla.innerHTML = ``
+    animLoader.innerHTML = ""
+    }, 3000);
+    
 })
 
 imputTarde.addEventListener(`change`,() => {
+  animLoader.innerHTML = `<div class="d-flex align-items-center">
+  <strong>Cargando...</strong>
+  <div class="spinner-grow ms-auto text-success" role="status" aria-hidden="true"></div>
+  </div>`
+
+setTimeout(() => {
     result.innerHTML = `<h4>Consultorios libres por la tarde</h4>
     <p>${consLibresTar}</p>`
     planilla.innerHTML = ``
+    animLoader.innerHTML = ""
+    }, 3000);
+    
 })
 
 
@@ -443,3 +508,17 @@ const myChart = new Chart(ctx, {
     }
 });
 
+const listado = document.getElementById("usuarios")
+
+fetch(`./js/datos.json`)
+  .then ((resp) => resp.json())
+  .then ((datos) => {
+    datos.forEach(usuario => {
+      const li = document.createElement(`li`)
+      li.innerHTML = `<h5>Usuario: ${usuario.User}</h5>
+        <p>Sector: ${usuario.Sector}</p>
+        <p>Es referente/encargado: ${usuario.Referente}</p>`
+
+      listado.append(li)
+    });
+  })
